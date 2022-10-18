9fa75b547bb440b785890f7c8a7b2c1951c78c62
ignis05 <48099798+ignis05@users.noreply.github.com>
2022-10-18 19:03:46 +0200

    feat: kkm mock insertion

--- src/app/components/kkm-screen/kkm-screen.component.ts
+++ src/app/components/kkm-screen/kkm-screen.component.ts
@@ -9,20 +9,29 @@ import { NavigateBack } from 'src/app/interfaces/navigate-back'
 	styleUrls: ['./kkm-screen.component.css'],
 })
 export class KkmScreenComponent implements OnInit, NavigateBack {
-	activeSubComponent: string = 'KkmSelect'
+	activeSubComponent: string = 'insertKKM'
 
 	constructor(public translate: TranslateService, private router: Router) {
 		this.goBack = this.goBack.bind(this)
+
+		this.kkmMockupHandler = this.kkmMockupHandler.bind(this)
+		document.addEventListener('keyup', this.kkmMockupHandler)
 	}
 
 	goBack(): void {
-		if (this.activeSubComponent === 'KkmSelect') this.router.navigateByUrl('/')
+		if (['KkmSelect', 'kkmRemoved'].includes(this.activeSubComponent)) this.router.navigateByUrl('/')
 		else if (['activeTicketsList', 'ticketActivation'].includes(this.activeSubComponent)) this.activeSubComponent = 'KkmSelect'
 	}
 
 	KkmSelectHandler(navigateTo: string) {
 		console.log(navigateTo)
 		this.activeSubComponent = navigateTo
+	}
+
+	kkmMockupHandler(e: KeyboardEvent) {
+		if (e.code !== 'Space') return
+		if (this.activeSubComponent === 'insertKKM') this.activeSubComponent = 'KkmSelect'
+		else this.activeSubComponent = 'kkmRemoved'
 	}
 
 	ngOnInit(): void {}
