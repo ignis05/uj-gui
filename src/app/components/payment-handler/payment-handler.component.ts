fe4487242c949d55dd92b8e8047d5a4247ce40ee
ignis05 <48099798+ignis05@users.noreply.github.com>
2022-10-21 15:14:24 +0200

    feat: credit card payment results

--- src/app/components/payment-handler/payment-handler.component.ts
+++ src/app/components/payment-handler/payment-handler.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnDestroy, OnInit } from '@angular/core'
+import { Router } from '@angular/router'
 
 @Component({
 	selector: 'app-payment-handler',
@@ -10,8 +11,11 @@ export class PaymentHandlerComponent implements OnInit, OnDestroy {
 	@Input() goBackEvent: EventEmitter<string> = new EventEmitter()
 	chosenPaymentMethod: 'cash' | 'card' | 'blik' | undefined
 	eventSubscription: any
+	cardPaymentResult: 'accepted' | 'declined' | undefined
 
-	constructor() {}
+	constructor(private router: Router) {
+		this.mockupInteractionHandler = this.mockupInteractionHandler.bind(this)
+	}
 
 	goBackFunc() {
 		if (!this.chosenPaymentMethod) this.goBackEvent.emit('toParent')
@@ -22,13 +26,31 @@ export class PaymentHandlerComponent implements OnInit, OnDestroy {
 		this.eventSubscription = this.goBackEvent.subscribe((e) => {
 			if (e === 'toChild') this.goBackFunc()
 		})
+		document.addEventListener('keyup', this.mockupInteractionHandler)
 	}
 
 	ngOnDestroy(): void {
 		this.eventSubscription?.unsubscribe?.()
+		document.removeEventListener('keyup', this.mockupInteractionHandler)
+	}
+
+	mockupInteractionHandler(e: KeyboardEvent) {
+		console.log(e.code)
+		if (this.chosenPaymentMethod == 'card') {
+			console.log('card')
+			if (e.code == 'KeyA') this.cardPaymentResult = 'accepted'
+			else if (e.code == 'KeyD') this.cardPaymentResult = 'declined'
+		}
 	}
 
 	paymentMethodSelectHandler(selected: typeof this.chosenPaymentMethod) {
 		this.chosenPaymentMethod = selected
+	}
+
+	cardPaymentCompleteHandler() {
+		if (this.cardPaymentResult === 'accepted') this.router.navigateByUrl('/')
+		else this.goBackFunc()
+
+		this.cardPaymentResult = undefined
 	}
 }
