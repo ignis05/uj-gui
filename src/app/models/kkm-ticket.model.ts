0eeccec4f5cf71d5f0b3bb376617ecb37d0160c4
ignis05 <48099798+ignis05@users.noreply.github.com>
2022-10-25 15:10:40 +0200

    feat: active tickets in grid

--- src/app/models/kkm-ticket.model.ts
+++ src/app/models/kkm-ticket.model.ts
@@ -11,4 +11,12 @@ export const MockKkmTickets: KkmTicket[] = [
 	{ activeSince: '10/09/2022', activeUntil: '10/10/2022', busLine: '116', isActive: false },
 	{ activeSince: '10/09/2022', activeUntil: '10/10/2022', busLine: 'all', isActive: false },
 	{ activeSince: '10/09/2022', activeUntil: '10/10/2022', busLine: '150', isActive: false },
+	{ activeSince: '10/09/2022', activeUntil: '10/10/2022', busLine: 'all', isActive: false },
+	{ activeSince: '10/09/2022', activeUntil: '10/10/2022', busLine: '116', isActive: false },
+	{ activeSince: '10/09/2022', activeUntil: '10/10/2022', busLine: 'all', isActive: false },
+	{ activeSince: '10/09/2022', activeUntil: '10/10/2022', busLine: '150', isActive: false },
+	{ activeSince: '10/09/2022', activeUntil: '10/10/2022', busLine: 'all', isActive: false },
+	{ activeSince: '10/09/2022', activeUntil: '10/10/2022', busLine: '116', isActive: false },
+	{ activeSince: '10/09/2022', activeUntil: '10/10/2022', busLine: 'all', isActive: false },
+	{ activeSince: '10/09/2022', activeUntil: '10/10/2022', busLine: '150', isActive: false },
 ]
