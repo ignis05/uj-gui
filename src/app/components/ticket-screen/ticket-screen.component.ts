05c9cee9fb3b23ad0c072e87dff61dcb1cb7637d
ignis05 <48099798+ignis05@users.noreply.github.com>
2022-11-03 13:41:58 +0100

    feat: ticketScreen functionality

--- src/app/components/ticket-screen/ticket-screen.component.ts
+++ src/app/components/ticket-screen/ticket-screen.component.ts
@@ -3,7 +3,7 @@ import { Router } from '@angular/router'
 
 import { TranslateService } from '@ngx-translate/core'
 import { NavigateBack } from 'src/app/interfaces/navigate-back'
-import { PaperTicket, possibleTickets } from 'src/app/models/paper-ticket.model'
+import { PaperTicket, possibleTickets, possibleTimes } from 'src/app/models/paper-ticket.model'
 
 @Component({
 	selector: 'app-ticket-screen',
@@ -14,15 +14,16 @@ export class TicketScreenComponent implements OnInit, NavigateBack {
 	visibleComponent: 'main' | 'payment' = 'main'
 	childGoBackEEmitter = new EventEmitter<string>()
 	cart: PaperTicket[] = [
-		{ reduced: true, validFor: '20 #minutes', zone: '1', price: 2.0 },
-		{ reduced: false, validFor: '20 #minutes', zone: '1', price: 5.0 },
+		{ reduced: true, validFor: '20 min', zone: 'I', price: 2.0 },
+		{ reduced: false, validFor: '20 min', zone: 'I', price: 5.0 },
 	]
 	ticketOptions = {
 		reduced: false,
-		zone: '1',
+		zone: 'I',
 		amount: 1,
 	}
 	possibleTickets = possibleTickets
+	possibleTimes = possibleTimes
 
 	constructor(public translate: TranslateService, private router: Router) {
 		this.goBack = this.goBack.bind(this)
@@ -46,5 +47,43 @@ export class TicketScreenComponent implements OnInit, NavigateBack {
 		this.visibleComponent = 'payment'
 	}
 
+	clearCart() {
+		this.cart = []
+	}
+
 	ngOnInit(): void {}
+
+	typeToggle() {
+		this.ticketOptions.reduced = !this.ticketOptions.reduced
+	}
+
+	zoneToggle() {
+		const zones = ['I', 'I + II', 'I + II + III']
+		let i = zones.indexOf(this.ticketOptions.zone)
+		if (i == -1) return console.error('invalid ticket zone')
+		this.ticketOptions.zone = zones[(i + 1) % zones.length]
+	}
+
+	setAmount(change: number) {
+		this.ticketOptions.amount = Math.min(Math.max(1, this.ticketOptions.amount + change), 20)
+	}
+
+	addToCard(validFor: string) {
+		let ticket: PaperTicket = {
+			price: 10,
+			reduced: this.ticketOptions.reduced,
+			zone: this.ticketOptions.zone,
+			validFor,
+		}
+
+		for (let i = 0; i < this.ticketOptions.amount; i++) {
+			this.cart.push(ticket)
+		}
+	}
+
+	removeFromCart(ticket: PaperTicket) {
+		let i = this.cart.findIndex((t) => t == ticket)
+		if (i == -1) return console.error('ticket to delete not found in cart')
+		this.cart.splice(i, 1)
+	}
 }
--- src/app/models/paper-ticket.model.ts
+++ src/app/models/paper-ticket.model.ts
@@ -5,4 +5,43 @@ export interface PaperTicket {
 	price: number
 }
 
-export const possibleTickets = ['20 min', '40 min', '1 h', '2 h', '24 h', '7 d']
+export const possibleTickets: PaperTicket[] = [
+	{
+		reduced: false,
+		validFor: '20 min',
+		zone: 'I',
+		price: 2.0,
+	},
+	{
+		reduced: false,
+		validFor: '40 min',
+		zone: 'I',
+		price: 3.0,
+	},
+	{
+		reduced: false,
+		validFor: '1 h',
+		zone: 'I',
+		price: 4.0,
+	},
+	{
+		reduced: false,
+		validFor: '2 h',
+		zone: 'I',
+		price: 5.0,
+	},
+	{
+		reduced: false,
+		validFor: '1 d',
+		zone: 'I',
+		price: 6.0,
+	},
+	{
+		reduced: false,
+		validFor: '7 d',
+		zone: 'I',
+		price: 13.0,
+	},
+]
+
+export const possibleTimes = Array.from(new Set(possibleTickets.map((t) => t.validFor)).values())
