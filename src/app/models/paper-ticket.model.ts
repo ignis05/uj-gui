79f25dee1d92b54e59170f6a348de95d8d554453
ignis05 <48099798+ignis05@users.noreply.github.com>
2022-11-08 14:06:19 +0100

    feat: more ticket types

--- src/app/models/paper-ticket.model.ts
+++ src/app/models/paper-ticket.model.ts
@@ -1,6 +1,6 @@
 export interface PaperTicket {
 	reduced: boolean
-	time: '#valid-20min' | '#valid-60min' | '#valid-90min' | '#valid-24h' | '#valid-48h' | '#valid-7d'
+	time: '#valid-20min' | '#valid-60min' | '#valid-90min' | '#valid-24h' | '#valid-48h' | '#valid-72h' | '#valid-7d' | '#valid-group'
 	zone: string
 	price: number
 }
@@ -44,6 +44,12 @@ export const possibleTickets: PaperTicket[] = [
 	},
 	{
 		reduced: false,
+		time: '#valid-72h',
+		zone: 'I + II + III',
+		price: 50.0,
+	},
+	{
+		reduced: false,
 		time: '#valid-7d',
 		zone: 'I',
 		price: 56.0,
@@ -53,6 +59,18 @@ export const possibleTickets: PaperTicket[] = [
 		time: '#valid-7d',
 		zone: 'I + II + III',
 		price: 68.0,
+	},
+	{
+		reduced: false,
+		time: '#valid-group',
+		zone: 'I',
+		price: 50.0,
+	},
+	{
+		reduced: false,
+		time: '#valid-group',
+		zone: 'I + II + III',
+		price: 60.0,
 	},
 	// reduced:
 	{
@@ -93,6 +111,12 @@ export const possibleTickets: PaperTicket[] = [
 	},
 	{
 		reduced: true,
+		time: '#valid-72h',
+		zone: 'I + II + III',
+		price: 25.0,
+	},
+	{
+		reduced: true,
 		time: '#valid-7d',
 		zone: 'I',
 		price: 28.0,
@@ -102,6 +126,18 @@ export const possibleTickets: PaperTicket[] = [
 		time: '#valid-7d',
 		zone: 'I + II + III',
 		price: 34.0,
+	},
+	{
+		reduced: true,
+		time: '#valid-group',
+		zone: 'I',
+		price: 25.0,
+	},
+	{
+		reduced: true,
+		time: '#valid-group',
+		zone: 'I + II + III',
+		price: 30.0,
 	},
 ]
 
